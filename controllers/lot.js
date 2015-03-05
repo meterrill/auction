923f9967d9db675b28a20e45b1049add2b98e01d
Matthew Terrill <meterrill@gmail.com>
2015-03-05 14:08:31 -0800

    add delete feature to lots

--- controllers/lot.js
+++ controllers/lot.js
@@ -3,6 +3,12 @@ Terrills.LotController = Ember.ObjectController.extend({
   actions: {
     showDetails: function() {
       this.set('detailsShowing', !this.get('detailsShowing'));
+    },
+
+    delete: function() {
+      if(confirm('Are you sure?')) {
+        this.get('model').destroyRecord();
+      }
     }
   }
 });
