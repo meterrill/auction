1bad093788546e34acc5cf3c9166fd39c8c93e7b
Luke Chinworth and Matthew Terrill <lukechinworth@gmail.com>
2015-03-05 15:13:48 -0800

    fix the delete function to delete the record from the parent too

--- controllers/auction.js
+++ controllers/auction.js
@@ -1,2 +1,13 @@
 Terrills.AuctionController = Ember.ObjectController.extend({
+  actions: {
+
+    deleteLot: function(lot) {
+      if(confirm("Are you sure?")) {
+        var auction = this.get('model');
+        auction.get('lots').removeObject(lot);
+        auction.save();
+        lot.destroyRecord();
+      }
+    }
+  }
 });
--- controllers/lot.js
+++ controllers/lot.js
@@ -3,12 +3,6 @@ Terrills.LotController = Ember.ObjectController.extend({
   actions: {
     showDetails: function() {
       this.set('detailsShowing', !this.get('detailsShowing'));
-    },
-
-    delete: function() {
-      if(confirm('Are you sure?')) {
-        this.get('model').destroyRecord();
-      }
     }
   }
 });
