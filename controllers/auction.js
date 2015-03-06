0a049ea8e336a3f521f4b52ff3ef629645721fcd
Matthew Terrill <meterrill@gmail.com>
2015-03-05 16:01:09 -0800

    add delete feature for auctions

--- controllers/auction.js
+++ controllers/auction.js
@@ -1,6 +1,14 @@
 Terrills.AuctionController = Ember.ObjectController.extend({
   actions: {
 
+    deleteAuction: function() {
+      if(confirm("Are you sure?")){
+        var auction = this.get('model');
+        auction.destroyRecord();
+        this.transitionToRoute('auctions');
+      }
+    },
+
     deleteLot: function(lot) {
       if(confirm("Are you sure?")) {
         var auction = this.get('model');
